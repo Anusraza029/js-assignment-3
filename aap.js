11fa2f8e03145793944f741a8b703d193e439a1e
Anusraza09 <sanus8339@gmail.com>
2023-11-21 00:22:49 +0600

    first commit

---
+++ aap.js
@@ -0,0 +1,25 @@
+var age ="I am 22 year old"
+alert(age);
+
+
+var visit ="You have visited this site 3 times"
+alert( visit);
+
+var birth = "My birth year is 2001"
+document.write(birth)
+var type = "Date type of my declared variable is number"
+document.write(type)
+
+
+var name ="</br><b>Ali </b>"
+var buy ="ordered"
+var quantity =" <b>5 T-shirt</b>"
+var store =" On imtiaz store"
+document.write(name+buy+quantity+store )
+
+
+
+
+
+
+   
